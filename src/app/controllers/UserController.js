c940d911120d175fad2eb5efd762d6bd85c48072
Huynh Minh Hoang <hoanghuynh0609@gmail.com>
2020-08-31 17:54:47 +0700

    fix login & register page

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
-
+const bcrypt = require('bcryptjs');
+//middalware
 class UserController {
     register(req, res) {
         res.render('user/register');
@@ -8,52 +9,79 @@ class UserController {
     login(req, res) {
         res.render('user/login');
     }
-    signin(req, res) {
-        const username = req.body.username;
-        const password = req.body.password;
-
-        User.findOne({ username: username, password: password })
-            .then((data) => {
-                if (data) {
-                    const token = jwt.sign(
-                        {
-                            _id: data._id,
-                        },
-                        'pass',
-                    );
-                    return res.json({
-                        message: 'Đăng nhập thành công',
-                        token: token,
-                    });
-                } else {
-                    res.status(300).json('Accout không đúng');
+    signin(req, res, next) {
+        const { username, password } = req.body;
+        if (!username || !password) {
+            return res
+                .status(500)
+                .json({ error: 'Vui lòng nhập đầy đủ thông tin!' });
+        }
+        User.findOne({ username: username })
+            .then((savedUser) => {
+                if (!savedUser) {
+                    return res
+                        .status(500)
+                        .json({
+                            error: 'Tên người dùng hoặc mật khẩu không hợp lệ!',
+                        });
                 }
+                bcrypt
+                    .compare(password, savedUser.password)
+                    .then((doMatch) => {
+                        if (doMatch) {
+                            // res.json({ message: "Đăng nhập thành công" });
+                            const token = jwt.sign(
+                                { _id: savedUser._id },
+                                'pass',
+                            );
+                            const { _id, username } = savedUser;
+                            res.json({ token, user: { _id, username } });
+                        } else {
+                            return res.status(422).json({
+                                error:
+                                    'Tên người dùng hoặc mật khẩu không hợp lệ!',
+                            });
+                        }
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    });
             })
             .catch((err) => {
-                res.status(500).json('Có lỗi bên server');
+                console.log(err);
             });
     }
-    signup(req, res) {
-        const username = req.body.username;
-        const password = req.body.password;
-        User.findOne({
-            username: username,
-        })
-            .then((data) => {
-                if (data) {
-                    res.json('Người dùng này đã tồn tại');
-                } else {
-                    return User.create({
-                        username: username,
-                        password: password,
-                    });
+    signup(req, res, next) {
+        const { username, password } = req.body;
+        if (!username || !password) {
+            return res
+                .status(500)
+                .json({ error: 'Vui lòng thêm tất cả các trường!' });
+        }
+        User.findOne({ username: username })
+            .then((savedUser) => {
+                if (savedUser) {
+                    return res
+                        .status(200)
+                        .json({ error: 'Người dùng này đã tồn tại!' });
                 }
-            })
-            .then((data) => {
-                res.redirect('/user/login');
+                bcrypt.hash(password, 12).then((hashedpassword) => {
+                    const user = new User({
+                        username,
+                        password: hashedpassword,
+                    });
+                    user.save()
+                        .then((user) => {
+                            //   res.json({ message: "Đăng ký thành công" });
+                            res.redirect('/user/login');
+                        })
+                        .catch((err) => {
+                            console.log(err);
+                        });
+                });
             })
             .catch((err) => {
-                res.status(500).json('Tạo tài khoản thất bại');
+                console.log(err);
             });
     }
 }
