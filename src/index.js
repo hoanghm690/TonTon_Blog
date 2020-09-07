7126de85429de11a72224f4f9c1eef52c439800e
Huynh Minh Hoang <hoanghuynh0609@gmail.com>
2020-09-07 16:45:41 +0700

    Sort middleware stored-courses

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const methodOverride = require('method-override');
 const handexphbs = require('express-handlebars');
 
+const SortMiddleware = require('./app/middlewares/SortMiddleware');
+
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
@@ -31,6 +33,8 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(methodOverride('_method'));
 
+app.use(SortMiddleware);
+
 // HTTP logger
 // app.use(morgan("combined"));
 
@@ -41,6 +45,25 @@ app.engine(
         extname: '.hbs',
         helpers: {
             sum: (a, b) => a + b,
+            sortable: (field, sort) => {
+                const sortType = field === sort.column ? sort.type : 'default';
+                const icons = {
+                    default: 'oi oi-elevator',
+                    asc: 'oi oi-sort-ascending',
+                    desc: 'oi oi-sort-descending',
+                };
+                const types = {
+                    default: 'desc',
+                    asc: 'desc',
+                    desc: 'asc',
+                };
+                const icon = icons[sortType];
+                const type = types[sortType];
+
+                return `<a href="?_sort&column=${field}&type=${type}">
+        <span class="${icon}"></span>
+        </a>`;
+            },
         },
     }),
 );
