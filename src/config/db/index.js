05fdcbee0da2b91b1c42f02a603759e5cf4df210
Huynh Minh Hoang <hoanghuynh0609@gmail.com>
2020-09-22 20:25:42 +0700

    connect db atlas

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,14 +2,11 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        await mongoose.connect(
-            'mongodb://localhost:27017/tonton_education_dev',
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-                useCreateIndex: true,
-            },
-        );
+        await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+        });
         console.log('Connect successfully!');
     } catch (error) {
         console.log('Connect failure!');
