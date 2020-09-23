b78c7275e1e4712742f62f3bce31e414c84a04c5
Huynh Minh Hoang <hoanghuynh0609@gmail.com>
2020-09-23 15:42:08 +0700

    update site

--- src/routes/site.js
+++ src/routes/site.js
@@ -5,6 +5,6 @@ const siteController = require('../app/controllers/SiteController');
 
 router.get('/search', siteController.search);
 router.get('/courses', requireLogin, siteController.courses);
-router.get('/', requireLogin, siteController.index);
+router.get('/', siteController.index);
 
 module.exports = router;
